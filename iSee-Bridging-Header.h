c60f898ff97f36f3658fb70ce5cf4766097355b9
Kamran <kamran.luminite@gmail.com>
2018-01-23 10:53:30 -0500

    Add files via upload

---
+++ iSee-Bridging-Header.h
@@ -0,0 +1,28 @@
+//
+//  iSee-Bridging-Header.h
+//  iSee
+//
+//  Created by Kamran Ali on 10/21/17.
+//  Copyright © 2017 KamranAliMSU. All rights reserved.
+//
+
+#ifndef iSee_Bridging_Header_h
+#define iSee_Bridging_Header_h
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "swix-Bridging-Header.h"
+#import <UIKit/UIKit.h>
+#import <DropboxSDK/DropboxSDK.h>
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for DataAccessWatch.
+FOUNDATION_EXPORT double DataAccessWatchVersionNumber;
+
+//! Project version string for DataAccessWatch.
+FOUNDATION_EXPORT const unsigned char DataAccessWatchVersionString[];
+
+
+#endif /* iSee_Bridging_Header_h */
